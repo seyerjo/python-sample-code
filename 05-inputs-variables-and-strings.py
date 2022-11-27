c30170b846a5eb636abd12b25a38727cb3d8b698
Jose Miguel Reyes <seyerjo@msn.com>
2022-11-27 12:52:35 +0000

    05-inputs-variables-and-strings.py | Comments are added to the code

--- 05-inputs-variables-and-strings.py
+++ 05-inputs-variables-and-strings.py
@@ -1,16 +1,25 @@
 """ GLOBAL VARIABLES """
+
+
+""" A global variable is declared """
 greeting_string = ""
 
 
 """ MAIN CODE """
+
+
+""" The username is requested in the console """
 nombre = input("What's your name?: ")
 print("\n")
 
+""" A string of characters is stored in the variable """
 greeting_string = "Hi " + nombre + ", my best regards."
 
+""" String is displayed in console """
 print(greeting_string)
 print("\n")
 
+""" The length of the string is calculated and a message is displayed in the console """
 print(
     "Did you know that the length of this greeting is "
     + str(len(greeting_string))
