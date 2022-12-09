491b7efe11219c839494c324d67b44dc8a9442cb
Jose Miguel Reyes <seyerjo@msn.com>
2022-12-09 17:17:11 +0000

    02-primitives-data-type.py | Comments in the code are updated

--- 02-primitives-data-type.py
+++ 02-primitives-data-type.py
@@ -1,16 +1,13 @@
-""" GLOBAL VARIABLES """
-
+#  GLOBAL VARIABLES
 
 my_integer = 1
 my_float = 1.0
 my_boolean = False
 my_none = None
 
+#  MAIN CODE
 
-""" MAIN CODE """
-
-
-""" The 'type' function returns the data type. """
+#  The 'type' function returns the data type.
 print(type(my_integer))
 print(type(my_float))
 print(type(my_boolean))
