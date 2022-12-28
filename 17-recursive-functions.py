37ade24df0d1f9de0571e9e53c67f558f0e9df1d
Jose Miguel Reyes <seyerjo@msn.com>
2022-12-28 17:11:10 +0000

    Code samples 17 and 18 added

---
+++ 17-recursive-functions.py
@@ -0,0 +1,68 @@
+#  GLOBAL VARIABLES
+
+
+#  Stores the number entered by the user.
+value = 0
+
+
+# FUNCTIONS
+
+
+def factorial(n):
+    """
+    Calculate the factorial of 'n'.
+
+    Args:
+        - n (integer) > 0.
+
+    The function returns factorial of 'n'.
+    If 'n' is equal to 1, this value is returned directly as a result of the function.
+    The function is recursive because it calls itself.
+
+    """
+
+    if n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+
+def request_number():
+    """
+    Prompts the user to enter an integer greater than zero.
+
+    Args:
+        - None.
+
+    The character entered by the user is converted to an integer.
+    The entered value is assigned to the global variable 'value'.
+    The function is recursive because it calls itself.
+
+    """
+
+    #  The user is prompted for the value.
+    value = int(input("Enter an integer greater than zero, please: "))
+
+    #  If the global variable 'value' is greater than zero, the factorial of the value
+    #  entered by the user is calculated and displayed on the console after a line break.
+
+    #  If the global variable 'value' is not greater or equal than zero, the user is
+    #  prompted again on the console after a line break.
+    if value > 0:
+        print("\n")
+        print("El factorial de " + str(value) + " es " + str(factorial(value)))
+    else:
+        print("\n")
+        request_number()
+
+
+#  MAIN CODE
+
+
+#  Presentation of the routine in the console between a pair of line breaks.
+print("\n")
+print(">>> Calculate the factorial of a number <<<")
+print("\n")
+
+# The user is prompted for a number.
+request_number()
---
+++ 18-recursion limit.py
@@ -0,0 +1,24 @@
+#  MAIN CODE
+
+
+import sys
+
+"""
+The default limit is 1000 recursive calls. It can be seen by calling
+'sys.getrecursionlimit()'. The maximum size of the recursion stack can be
+changed using the 'sys.setrecursionlimit(n)' statement. However, if this
+limit is being reached, it is usually a good idea to think about whether
+the recursive algorithm is really the one that best solves the problem.
+
+"""
+
+print(sys.getrecursionlimit())
+print("\n")
+
+sys.setrecursionlimit(999)
+print(sys.getrecursionlimit())
+print("\n")
+
+sys.setrecursionlimit(1000)
+print(sys.getrecursionlimit())
+print("\n")
