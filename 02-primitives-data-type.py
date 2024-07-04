5d3f6d0587a8a305f44e3a0aaeb0594402cce82a
seyerjo <seyerjo@msn.com>
2024-07-04 20:52:18 +0100

    Code improvement

--- 02-primitives-data-type.py
+++ 02-primitives-data-type.py
@@ -1,13 +1,25 @@
 #  MAIN CODE
 
-#  Variables with some primitive data types
+#  Variables with primitive data types
 my_integer = 1
-my_float = 1.0
+my_float = 3.14
+my_complex_number = 1 + 2j
+my_string = "Hello, Coder!"
+my_list = [1, 2, 3]
+my_tuple = (4.0, 5.5, 6.9)
+my_dictionary = {"key": "value"}
+my_set = {7, 8, 9}
 my_boolean = False
 my_none = None
 
 #  The 'type' function returns the data type.
-print(type(my_integer))
-print(type(my_float))
-print(type(my_boolean))
-print(type(my_none))
+print(type(my_integer))         #  Integer
+print(type(my_float))           #  Float       
+print(type(my_complex_number))  #  Complex
+print(type(my_string))          #  String
+print(type(my_list))            #  List
+print(type(my_tuple))           #  Tuple
+print(type(my_dictionary))      #  Dictionary
+print(type(my_set))             #  Set
+print(type(my_boolean))         #  Boolean
+print(type(my_none))            #  NoneType
