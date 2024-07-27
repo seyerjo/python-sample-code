81a97c002b984b151cb538ff3666eecb40c80e17
seyerjo <seyerjo@msn.com>
2024-07-27 18:52:49 +0100

    An example of valid variable names is added and code comments are improved.

--- 05-inputs-variables-and-strings.py
+++ 05-inputs-variables-and-strings.py
@@ -1,6 +1,43 @@
 #  FUNDAMENTALS
 
-#  Python has dynamic typing
+#  Best practices for variable names in Python.
+'''
+    . The name may not begin with a number
+    . The use of hyphens ‘-’ is not allowed.
+    . Spaces are not allowed.
+'''
+
+#  Invalid.
+'''
+    2variable = 10
+    var-iable = 10
+    var iable = 10
+'''
+
+#  Valid.
+_variable = 1
+varia_ble = 2.50
+variable5 = "string"
+myVariable = "33"
+
+"""
+    As a good practice it is advisable to name variables in lower case
+    and with underscores ‘_’, also known as ‘snake case’.
+"""
+variable_one = 1
+variable_two = 2.50
+variable_three = "string"
+variable_four = "33"
+
+
+#  Language reserved words may not be used as variable names.
+#  This code shows them in the console.
+import keyword
+print(keyword.kwlist)
+print("\n")
+
+
+#  Python has dynamic typing.
 variable_x = 1  # Integer
 variable_y = 3.75  # Float
 variable_z= "characters string"  # String
@@ -14,6 +51,7 @@ variable_z = 4.25 # Now is float
 print(variable_x, variable_y, variable_z)
 print(type(variable_x), type(variable_y), type(variable_z))
 print("\n")
+
 
 #  SAMPLE MAIN CODE
 
