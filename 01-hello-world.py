bddb64a9c861f0175a8b12f32aa75424e6288b68
seyerjo <seyerjo@msn.com>
2024-07-27 17:55:56 +0100

    Added changes to comments and main code

--- 01-hello-world.py
+++ 01-hello-world.py
@@ -1,4 +1,22 @@
-#  MAIN CODE
+#   FUNDAMENTALS
 
-#  'Hello World' is displayed in console.
+#   This is a one-line comment
+
+"""
+    This is a multi-line comment with double inverted commas
+"""
+
+'''
+    This is a multi-line comment with single inverted commas
+'''
+
+#   MAIN CODE
+
+"""
+    Hello World! is displayed in console. We can use single
+    or double inverted commas to wrap the text string to be
+    displayed.
+"""
+
 print("Hello World!")
+print('Hello World!')
